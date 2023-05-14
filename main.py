1ea315d61210b942369268fe7f254e43edc73716
Rock Chin <1010553892@qq.com>
2023-05-14 20:34:42 +0800

    feat: 基本功能

--- main.py
+++ main.py
@@ -1,49 +1,47 @@
 from pkg.plugin.models import *
 from pkg.plugin.host import EventContext, PluginHost
 
-"""
-在收到私聊或群聊消息"hello"时，回复"hello, <发送者id>!"或"hello, everyone!"
-"""
+import requests
+import random
+
+from mirai import Image
+import logging
 
 
 # 注册插件
-@register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
-class HelloPlugin(Plugin):
+@register(name="Nahida", description="Hello Nahida", version="0.1", author="RockChinQ")
+class NahidaPlugin(Plugin):
 
+    images_urls: list[str] = []
     # 插件加载时触发
     # plugin_host (pkg.plugin.host.PluginHost) 提供了与主程序交互的一些方法，详细请查看其源码
     def __init__(self, plugin_host: PluginHost):
-        pass
+        json_resp = requests.get(
+            url="https://api.github.com/repos/RockChinQ/NahidaImages/contents/images",
+        )
+        obj_json = json_resp.json()
 
-    # 当收到个人消息时触发
-    @on(PersonNormalMessageReceived)
-    def person_normal_message_received(self, event: EventContext, **kwargs):
-        msg = kwargs['text_message']
-        if msg == "hello":  # 如果消息为hello
+        for item in obj_json:
+            self.images_urls.append(item["download_url"])
 
-            # 输出调试信息
-            logging.debug("hello, {}".format(kwargs['sender_id']))
+        
+    @on(PersonMessageReceived)
+    @on(GroupMessageReceived)
+    def _(self, event: EventContext, host: PluginHost, message_chain, **kwargs):
+        text = str(message_chain).strip()
+        if text == "nahida" or text == "nhd":
+            # 发送图片
+            image_url = random.choice(self.images_urls)
+            
+            if kwargs["launcher_type"] == "group":
+                host.send_group_message(kwargs["launcher_id"], Image(url=image_url))
+            else:
+                host.send_person_message(kwargs["launcher_id"], Image(url=image_url))
 
-            # 回复消息 "hello, <发送者id>!"
-            event.add_return("reply", ["hello, {}!".format(kwargs['sender_id'])])
-
-            # 阻止该事件默认行为（向接口获取回复）
             event.prevent_default()
+            event.prevent_postorder()
 
-    # 当收到群消息时触发
-    @on(GroupNormalMessageReceived)
-    def group_normal_message_received(self, event: EventContext, **kwargs):
-        msg = kwargs['text_message']
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            logging.debug("hello, {}".format(kwargs['sender_id']))
-
-            # 回复消息 "hello, everyone!"
-            event.add_return("reply", ["hello, everyone!"])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            event.prevent_default()
+            logging.info("Nahida!")
 
     # 插件卸载时触发
     def __del__(self):
